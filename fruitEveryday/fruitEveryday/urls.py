8fabddd2aff0c4931db27cc9294dd492cd17d6fc
xwp <xwp_fullstack@163.com>
2016-10-31 07:52:46 +0800

    add search in full by zhangxy 	新文件：   fruitEveryday/foods/search_indexes.py 	新文件：   fruitEveryday/foods/search_indexes.pyc 	修改：     fruitEveryday/fruitEveryday/urls.py 	修改：     fruitEveryday/templates/foods/base.html 	修改：     fruitEveryday/templates/foods/userCenterBase.html 	新文件：   fruitEveryday/templates/search/indexes/foods/productinfo_text.txt 	新文件：   fruitEveryday/templates/search/search.html 	新文件：   fruitEveryday/whoosh_index/MAIN_6y3yrpt1p0kdthb6.seg 	新文件：   fruitEveryday/whoosh_index/MAIN_WRITELOCK 	新文件：   fruitEveryday/whoosh_index/_MAIN_1.toc

--- fruitEveryday/fruitEveryday/urls.py
+++ fruitEveryday/fruitEveryday/urls.py
@@ -19,4 +19,5 @@ from django.contrib import admin
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
     url(r'^', include('foods.urls', namespace='main')),
+    url(r'^search/', include('haystack.urls')),
 ]
